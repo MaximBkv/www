01b40572b4d21af4f2b9634712d91e36384c86bf
MaximBkv <bokov.max.s5@gmail.com>
2022-10-11 13:10:37 +0000

    first commit

---
+++ .top.menu.php
@@ -0,0 +1,39 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Главная страница", 
+		"/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Объявления", 
+		"advertisement/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"О сервисе", 
+		"o-servise/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Личный кабинет продавца", 
+		"lichnyy-kabinet-prodavtsa/", 
+		Array(), 
+		Array(), 
+		"CSite::InGroup(array(7))" 
+	),
+	Array(
+		"Личный кабинет покупателя", 
+		"lichnyy-kabinet-pokupatelya/", 
+		Array(), 
+		Array(), 
+		"CSite::InGroup(array(6))" 
+	)
+);
+?>
